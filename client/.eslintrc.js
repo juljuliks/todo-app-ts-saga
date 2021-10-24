fccc264bf74f2220d560e688a52af8f090f4dae7
Юлия Глухова <juliaglukhova666@gmail.com>
2021-10-24 22:56:09 +0400

    heroku build

--- client/.eslintrc.js
+++ client/.eslintrc.js
@@ -2,9 +2,11 @@ module.exports = {
   env: {
     browser: true,
     es2021: true,
+    node: true,
   },
   extends: [
     'plugin:react/recommended',
+    'react-app',
     'airbnb',
   ],
   parser: '@typescript-eslint/parser',
@@ -20,5 +22,32 @@ module.exports = {
     '@typescript-eslint',
   ],
   rules: {
+    'no-console': 'off',
+    'no-param-reassign': 'off',
+    'react/jsx-no-bind': 'off',
+    'react/jsx-filename-extension': 'off',
+    'no-use-before-define': 'off',
+    '@typescript-eslint/no-use-before-define': ['error'],
+    'react/react-in-jsx-scope': 'off',
+    'react/prop-types': 'off',
+    'no-unused-vars': 'off',
+    '@typescript-eslint/no-unused-vars': 'off',
+    'import/extensions': [
+      'error',
+      'ignorePackages',
+      {
+        tsx: 'never',
+        jsx: 'never',
+        js: 'never',
+        ts: 'never',
+      },
+    ],
+  },
+  settings: {
+    'import/resolver': {
+      node: {
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+      },
+    },
   },
 };
