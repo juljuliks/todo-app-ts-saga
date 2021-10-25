f70e6796dd4780b7af2547b540173e5697ed33e1
Юлия Глухова <juliaglukhova666@gmail.com>
2021-10-25 16:47:49 +0400

    heroku build

--- server/server.js
+++ server/server.js
@@ -18,15 +18,25 @@ const sessionConfig = {
   },
 };
 
+const buildHtml = path.resolve(process.env.PWD, '..', 'client', 'build', 'index.html');
+const buildStatic = path.resolve(process.env.PWD, '..', 'client', 'build');
+const serverStatic = path.resolve(process.env.PWD, 'public');
+
 const app = express();
 app.use(session(sessionConfig));
 
 app.use(logger('dev'));
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(buildStatic));
+app.use(express.static(serverStatic));
 app.use(express.json());
 
 app.use('/api/todos', todosRouter);
 app.use('/api', authRouter);
+
+app.get('*', (req, res) => {
+  res.sendFile(buildHtml);
+});
 
 app.listen(5000, () => {
   console.log('server started');
