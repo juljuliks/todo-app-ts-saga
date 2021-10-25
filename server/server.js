3bd1b2bdfe5975d8ef28f31d1c16e6b3665f38d2
Юлия Глухова <juliaglukhova666@gmail.com>
2021-10-25 17:16:33 +0400

    heroku build

--- server/server.js
+++ server/server.js
@@ -38,6 +38,8 @@ app.get('*', (req, res) => {
   res.sendFile(buildHtml);
 });
 
-app.listen(5000, () => {
+const PORT = process.env.PORT ?? 5000;
+
+app.listen(PORT, () => {
   console.log('server started');
 });
